a23e3a7c80ba371bd300ab9b64c99bee1a751fe7
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-06 22:25:59 +0200

    Added report finder utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from "./configuration-loader.util";
+export * from "./report-finder.util";
---
+++ src/utils/report-finder.util.ts
@@ -0,0 +1,51 @@
+import { Project, Report } from "@model";
+
+import * as fs from "fs";
+import * as path from "path"
+
+
+export class ReportFinder
+{
+    public static execute(project: Project) : Report[]
+    {
+        const reports: Report[] = [];
+        const inputReportFilepaths = this.getFolderFiles(project.inputFolderPath);
+        for (const filepath of inputReportFilepaths)
+        {
+            if (filepath.endsWith(".json") || filepath.endsWith(".xml"))
+            {
+                reports.push({filepath});
+            }
+        }
+
+        return reports;
+    }
+
+    private static getFolderFiles(folderPath: string, recursive = false): string[]
+    {
+        const files = [];
+        if (fs.existsSync(folderPath))
+        {
+            const folderEntries = fs.readdirSync(folderPath);
+            for (const folderEntry of folderEntries)
+            {
+                const folderEntryPath = `${folderPath}/${folderEntry}`;
+                if (fs.lstatSync(folderEntryPath).isDirectory())
+                {
+                    if (recursive)
+                    {
+                        const subfolderFiles = this.getFolderFiles(folderEntryPath);
+                        const sufolderFilesRelativePaths = subfolderFiles.map((p) => path.join(folderEntry, p) );
+                        files.push(...sufolderFilesRelativePaths);
+                    }
+                }
+                else
+                {
+                    files.push(folderEntry);
+                }
+            }
+        }
+
+        return files;
+    }
+}
