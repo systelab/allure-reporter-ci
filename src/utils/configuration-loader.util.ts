968b0f6de60a6d1b5c5869c09da5014cfb367146
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-06 21:02:02 +0200

    Added utility to load configuration files

---
+++ src/utils/configuration-loader.util.ts
@@ -0,0 +1,13 @@
+import { Configuration } from "@model";
+
+import * as fs from "fs";
+
+
+export class ConfigurationLoader
+{
+    public static load(filepath: string) : Configuration
+    {
+        const fileContent = fs.readFileSync(filepath).toString();
+        return JSON.parse(fileContent);
+    }
+}
