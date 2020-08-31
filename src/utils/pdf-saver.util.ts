f7831210fb7877bf88b5830b5822ec5aab38e1ce
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-31 19:24:18 +0200

    Used filesystem instead of using native filesystem methods.

--- src/utils/pdf-saver.util.ts
+++ src/utils/pdf-saver.util.ts
@@ -1,8 +1,7 @@
+import * as puppeteer from "puppeteer";
+
 import { Project, ReportContent } from "@model";
 import { FilesystemUtility } from "@utils";
-
-import * as path from "path";
-import * as puppeteer from "puppeteer";
 
 
 export class PDFSaver
@@ -18,27 +17,18 @@ export class PDFSaver
         await page.pdf({ path: pdfFilepath, format: "A4" });
     }
 
-    public static cleanOldOutputFiles(outputFolderPath: string)
-    {
-        if (FilesystemUtility.exists(outputFolderPath))
-        {
-            FilesystemUtility.deleteFolderContents(outputFolderPath);
-        }
-    }
-
     private static getUniquePDFFilepath(folderPath: string, inputReportContent: ReportContent): string
     {
         const filename: string = `${inputReportContent.tmsId}_${inputReportContent.name}`.replace(/[/\\ ]/g, "_");
 
         let counter = 2;
-        let pdfFilepath: string = path.join(folderPath, `${filename}.pdf`);
+        let pdfFilepath: string = FilesystemUtility.joinPaths(folderPath, `${filename}.pdf`);
         while (FilesystemUtility.exists(pdfFilepath))
         {
-            pdfFilepath = path.join(folderPath, `${filename}_${counter}.pdf`);
+            pdfFilepath = FilesystemUtility.joinPaths(folderPath, `${filename}_${counter}.pdf`);
             counter++;
         }
 
         return pdfFilepath;
     }
-
 }
