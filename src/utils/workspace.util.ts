9f993a3dd944ff0ddf948cbbe13803dc646f7b38
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-31 20:18:40 +0200

    Added method to get workspace path

--- src/utils/workspace.util.ts
+++ src/utils/workspace.util.ts
@@ -4,6 +4,12 @@ import { FilesystemUtility } from "@utils";
 
 export class WorkspaceUtility
 {
+    public static buildPath(relativePath: string)
+    {
+        const workspaceRootPath = process.env.ALLURE_REPORTER_CI_WORKSPACE || "";
+        return FilesystemUtility.joinPaths(workspaceRootPath, relativePath);
+    }
+
     public static cleanOldOutputFiles(project: Project)
     {
         if (FilesystemUtility.exists(project.outputFolderPath))
