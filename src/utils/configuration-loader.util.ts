5dc9d219e0b47c7fbcc70d37c280e380ff2ceb6a
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-31 20:06:46 +0200

    Polished retrieval of configuration file

--- src/utils/configuration-loader.util.ts
+++ src/utils/configuration-loader.util.ts
@@ -7,10 +7,10 @@ export class ConfigurationLoader
     public static load(): Configuration
     {
         const workspace = process.env.ALLURE_REPORTER_CI_WORKSPACE || "";
-        const filepath = process.env.ALLURE_REPORTER_CI_CONFIGURATION || "test/configuration/configuration-all.json";
-        const fullFilepath = FilesystemUtility.joinPaths(workspace, filepath);
+        const configurationFile = process.env.ALLURE_REPORTER_CI_CONFIG_FILE || "test/configuration/configuration-all.json";
+        const configurationFilepath = FilesystemUtility.joinPaths(workspace, configurationFile);
 
-        return this.loadFromFilepath(fullFilepath);
+        return this.loadFromFilepath(configurationFilepath);
     }
 
     public static loadFromFilepath(filepath: string): Configuration
